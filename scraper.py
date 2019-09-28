a0533e03ea6b9509cea8aa3878772e465294ec22
canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2019-09-28 17:41:58 -0500

    Created scrap function

--- scraper.py
+++ scraper.py
@@ -3,6 +3,33 @@ from bs4 import BeautifulSoup
 
 url = 'https://www.listademaestros.com/fime/buscar'
 
+def scrap(url):
+    response = requests.get(url)
+    if response:
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        def find(id):
+            return soup.find(id = id)
+
+        scores = [
+                find('ex_li'),
+                find('ac_li'),
+                find('pa_li'),
+                find('as_li'),
+                find('se_li')
+                ]
+
+        rates = [
+                find('chidos_score'),
+                find('gachos_score')
+                ]
+
+        for score in scores:
+            print(score)
+
+        for rate in rates:
+            print(rate)
+
 def find(name : str):
     if not name:
         return None
@@ -16,7 +43,11 @@ def find(name : str):
 
         table = soup.find(id = 'nombres_table')
         for link in table.find_all('a'):
-            result.append(link['href'])
+            href = link['href']
+            maestro = scrap(href)
+
+            result.append(maestro)
+            result.append(href)
 
         if len(result) == 0:
             return None
